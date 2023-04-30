7646a95ae094dc073c0e83c9514d8b0bc5688a6e
navneeet94 <98764448+navneeet94@users.noreply.github.com>
2023-04-30 22:51:14 +0530

    update code

--- projects/node/bloggy/ctrls/front.ctrl.js
+++ projects/node/bloggy/ctrls/front.ctrl.js
@@ -87,133 +87,8 @@ module.exports.postRegister = (req, res) => {
     const email = req.body.email;
     const pwd = req.body.pwd;
 
-    const mailer_html = `
-    <!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to Bloggy</title>
-</head>
-
-<body style="margin: 0;font-family: 'system-ui', sans-serif , arial;">
-    <table cellspacing="0" cellpading="0" width="700px" style="max-width: 100%;margin: 30px auto;border: 1px solid #ccc;">
-        <!-- upper border -->
-        <tr>
-            <td>
-                <table width="100%" cellspacing="0" cellpading="0">
-                    <tr>
-                        <td style="width: 100%;height: 3px;background: linear-gradient(to right, #0267C1, #D65108);"></td>
-                    </tr>
-                </table>
-            </td>
-        </tr>
-        <!-- User Info -->
-        <tr>
-            <td>
-                <table width="100%" cellspacing="0" cellpading="0" style="box-shadow: 0 10px 20px -5px rgba(0, 0, 0, 0.05);border-radius: 0 0 50% 50%/1%;">
-                    <tr>
-                        <td style="text-align: center;padding-top: 40px;">
-                            <span style="display: block;color: #0267C1;font-weight: 600;">Hello, ${name}</span>
-                            <h1 style="font-size: 20px;font-weight: 600;color: #333;margin-top: 1px;">Thank you for Signing Up</h1>
-                            <p style="font-size: 14px;margin-bottom:5px">Your Username :- <span style="color: #0267C1;">${username}</span></p>
-                            <p style="font-size: 14px;margin-top:0">Your Password :- <span style="color: #0267C1;">${pwd}</span></p>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td style="text-align: center;padding-top: 20px;padding-bottom: 15px;">
-                            <img src="https://i.ibb.co/2g7tS2d/good.png" alt="good" border="0" style="width: 85px;display: block;margin: 0 auto;">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td style="text-align: center;color: slategrey;font-size: 20px;font-weight: 600;padding-bottom: 20px;">Bloggy is here</td>
-                    </tr>
-                </table>
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <table width="100%" cellspacing="0" cellpading="0">
-                    <tr>
-                        <td style="width: 8%"></td>
-                        <td style="width: 84%;padding: 20px 0px;color: slategrey;">
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus esse fugiat, repudiandae alias sequi cupiditate quasi nostrum eveniet, debitis, facere nesciunt quia animi eius nobis quas ipsa odio explicabo quis obcaecati. Voluptatum harum deserunt inventore quos, nihil dicta libero autem maxime dolorum rem dolorem recusandae aspernatur molestias debitis obcaecati minima commodi, veniam facere quia.
-                            </p>
-                            <p>
-                                <span style="color: steelblue;">Hold up, there's more!</span>
-                                A 7 days simulation trial, your trial starts now.
-                            </p>
-                            <p>If you have any questions, kindly reach out to our team on support@placeholder.com</p>
-                            <p>Have an AWESOME day! <br>
-                                Brought to you by your friends at Placeholder.
-                            </p>
-                        </td>
-                        <td style="width: 8%"></td>
-                    </tr>
-                </table>
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <table width="100%" cellspacing="0" cellpading="0">
-                    <tr>
-                        <td>
-                            <h2 style="text-align: center;margin-top: 0;font-weight: 500;">What we offer</h2>
-                        </td>
-                    </tr>
-                </table>
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <table width="100%" cellspacing="0" cellpading="0" style="table-layout: fixed;">
-                    <tr>
-                        <td style="width: 10%"></td>
-                        <td style="width: 40%;text-align: center;padding: 0 10px;">
-                            <span style="display: block;width: 50px;margin: 0 auto;"><img src="https://i.ibb.co/tHZQHTB/desktop.png" alt="desktop" border="0" width="100%" ></span>
-                            <strong style="font-size: 16px;">Advance Backend</strong>
-                            <p style="font-size: 13px;color: slategrey;">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet sed culpa cupiditate?</p>
-                        </td>
-                        <td style="width: 40%;text-align: center;padding: 0 10px;">
-                            <span style="display: block;width:50px;margin: 0 auto;"><img src="https://i.ibb.co/zSD3NkX/smartphone.png" alt="smartphone" border="0" width="100%"></span>
-                            <strong style="font-size: 16px;">Mobile Platform</strong>
-                            <p style="font-size: 13px;color: slategrey;">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet sed culpa cupiditate?</p>
-                        </td>
-                        <td style="width: 5%"></td>
-                    </tr>
-                </table>
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <table width="100%" cellspacing="0" cellpading="0">
-                    <tr>
-                        <td style="text-align: center;padding-top: 40px; padding-bottom: 40px;">
-                            <a href="#" style="padding: 15px 25px;font: inherit;background: linear-gradient(to right, #0267C1, #0280EF);border-radius: 50px;border: 0;color: #fff;display: inline-block;text-decoration: none;">Explore</a>
-                        </td>
-                    </tr>
-                </table>
-            </td>
-        </tr>
-        <tr>
-            <td style="text-align: center;color: slategrey;font-size: 14px;padding-bottom: 40px;">
-                Bloggy © 2023
-                <br />
-                Tel: 00 1 460 5416
-                <br />
-                Somewhere in earth
-            </td>
-        </tr>
-    </table>
-</body>
-
-</html>
-    `
-
     let otp_str = String(Math.floor(1000 + Math.random() * 9000));
+    console.log(otp_str);
     let otp_chars = otp_str.split('');
 
     const otpVerification_html = `<!DOCTYPE html>
@@ -282,8 +157,6 @@ module.exports.postRegister = (req, res) => {
     
     </html>`
 
-    
-
     let otpVerification = {
         from: 'navneetaneja.ds@gmail.com',
         to: email,
@@ -292,20 +165,18 @@ module.exports.postRegister = (req, res) => {
         html: otpVerification_html
     };
 
-    // let mailOptions = {
-    //     from: 'navneetaneja.ds@gmail.com',
-    //     to: email,
-    //     subject: 'Welcome To Bloggy',
-    //     text: 'Thanks for your interest in Bloggy',
-    //     html: mailer_html
-    // };
-
     transporter.sendMail(otpVerification, (err, info) => {
         if (err) {
             console.log(err)
         }
         else {
-            console.log("Email sent:" + info.response)
+            req.session.isOtp = true;
+            req.session.otpSend = otp_str;
+            req.session.sendUserName = username;
+            req.session.sendEmail = email;
+            req.session.sendName = name;
+            req.session.sendPwd = pwd;
+            console.log("Email sent:" + info.response);
         }
     })
 }
