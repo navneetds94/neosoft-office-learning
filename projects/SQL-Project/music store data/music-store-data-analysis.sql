270c710d191022c190c33bc0f57201ce180e7d72
navneeet94 <98764448+navneeet94@users.noreply.github.com>
2023-05-15 18:54:45 +0530

    update code

--- projects/SQL-Project/music store data/music-store-data-analysis.sql
+++ projects/SQL-Project/music store data/music-store-data-analysis.sql
@@ -1,12 +1,13 @@
 use music;
 show tables;
--- Who is the senior most employee based on job title?
+
+-- 1.) Who is the senior most employee based on job title?
 select 
 concat(first_name," ",last_name) 
 FROM employee 
 order by levels desc limit 1;
 
--- Which Country has the most invoices?
+-- 2.) Which Country has the most invoices?
 SELECT
 billing_country,
 COUNT(billing_country) 
@@ -15,13 +16,13 @@ group by 1
 order by 2 desc
 limit 1;
 
--- what are top 3 values of total invoices
+-- 3.) what are top 3 values of total invoices
 SELECT * FROM invoice
 order by total desc
 limit 3;
 
--- which city has the best customers? we would like to throw a promotional music festival
--- in the city we made most money. WAQ that return both the city name and sum of all invoices total
+/* 4.) which city has the best customers? we would like to throw a promotional music festival
+in the city we made most money. WAQ that return both the city name and sum of all invoices total */
 SELECT
 billing_city , round(sum(total),2) as invoice_total
 FROM customer tablea
@@ -30,7 +31,7 @@ on tablea.customer_id = tableb.customer_id
 group by 1
 order by 2 desc;
 
-/* Q5: Who is the best customer? The customer who has spent the most money will be declared the best customer. 
+/* 5.) : Who is the best customer? The customer who has spent the most money will be declared the best customer. 
 Write a query that returns the person who has spent the most money.*/
 SELECT
 concat(first_name," ",last_name) , round(sum(total),2) as invoice_total
@@ -41,5 +42,113 @@ group by 1
 order by 2 desc
 limit 1;
 
-/* WAQ to return the email , first name , last name , Genre of all rock music listners.
+/* 6.) WAQ to return the email , first name , last name of all rock music listners.
 Return your list order alphabeticaly by email from A-Z */
+SELECT
+distinct email , first_name , last_name 
+-- , trc.track_id , gen.genre_id
+FROM customer cust
+join invoice inv
+on inv.customer_id = cust.customer_id
+join invoice_line invline
+on invline.invoice_id = inv.invoice_id
+join track trc
+on trc.track_id = invline.track_id
+join genre gen
+on gen.genre_id = trc.genre_id
+where lower(gen.name) = "rock"
+order by 1;
+
+/* 
+7. Lets invite the artist who have written the most rock music in our dataset .
+WAQ that returns the Artist name and total track count of the top 10 rock bands
+*/
+with artist_rank as ( SELECT 
+alb.artist_id , count(alb.artist_id) as rock_songs
+FROM albums alb
+join track trc
+on trc.album_id = alb.album_id
+join genre gen
+on gen.genre_id = trc.genre_id
+where lower(gen.name) like 'rock'
+group by 1
+order by 2 desc )
+
+-- Query to fetch and return names
+select
+name , rock_songs,
+dense_rank() OVER(ORDER BY rock_songs desc) ranking
+from artist_rank
+inner join artist art
+on art.artist_id = artist_rank.artist_id
+limit 10;
+
+/*
+8. Return all the track names that have a song length longer than the average song length.
+Return the name and millesconds of each track.
+order by the song length with longest words list first.
+*/
+
+SELECT
+Name , milliseconds
+FROM track
+where milliseconds > (select avg(milliseconds) from track)
+order by 2 desc;
+
+/*
+9. Find how much amount spent by each customer on artists?
+WAQ to return customer name, artist name and total spent
+ */
+ 
+ with custome_artist_data as ( SELECT
+ concat(customer.first_name," ",customer.last_name) as custome_name , artist.name , invoice.total
+ FROM customer
+ join invoice
+ on invoice.customer_id = customer.customer_id
+ join invoice_line
+ on invoice_line.invoice_id = invoice.invoice_id
+ join track
+ on track.track_id = invoice_line.track_id
+ join albums
+ on albums.album_id = track.album_id
+ join artist
+ on artist.artist_id = albums.artist_id )
+ 
+ SELECT 
+ distinct * 
+ FROM custome_artist_data
+ group by 1 , 2 , 3
+ order by sum(total) desc
+ limit 10;
+ 
+/*
+10 We want to find out the most popular music genre for each country.
+we determine the most popular genre as the genre with highest amount of purchase.
+WAQ that returns each country along with the top genre.
+ */
+ with genre_popular_count as ( SELECT
+ customer.country , genre.name
+ FROM customer
+ join invoice
+ on invoice.customer_id = customer.customer_id
+ join invoice_line
+ on invoice_line.invoice_id = invoice.invoice_id
+ join track
+ on track.track_id = invoice_line.track_id
+ join genre
+ on genre.genre_id = track.genre_id)
+ 
+SELECT
+country , name
+from ( SELECT 
+ * , count(name) as genre_count ,
+ rank() over(partition by country order by count(name) desc) as ranking
+ FROM genre_popular_count
+group by 1 , 2) subq
+where subq.ranking = 1;
+
+/*
+11. WAQ that determines the customer that has spent the most on music for each countr.
+WAQ that returns the country along with the top customers and how much they spent.
+For the country top amount is shared provide all customers who spent this amount
+*/
---
+++ projects/power-bi/HR-DASBOARD-POWER-BI-main/README.md
@@ -0,0 +1,7 @@
+# HR-DASBOARD-POWER-BI
+## Analytical Approach:-
+1. unpivot columns 2. create new messures 3. create a dynamic dashboard 4.  create columns by using DAX
+## KPI:- 
+1.Over All Present in percentage 2. Work From Home percentage 3. Sick Leave percentage 4. present of a one employee In percentage 5.Monthly Present percentage
+## insights :- 
+1. Most Pepole Present on Monday 2. Most Work From Home on Friday 3. In april Month Presents Are declined
